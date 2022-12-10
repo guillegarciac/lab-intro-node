14afc7e98941e2975397c495e1125ab4c8cb4d89
guille garcia <ggarciacardiel@gmail.com>
2022-12-10 14:16:44 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length; 
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length <= pos) {
+      throw new Error('OutOfBounds')
+    } else {return this.items[pos]}
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let result = 0;
+    if (this.items.length === 0) return 0;
+    for (let i = 0; i < this.items.length; i++) {
+      result += this.items[i];
+    }
+    return result;
+  }
 
-  avg() {}
+  avg() {
+    let sumItems = 0;
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    if (this.items.length === 1) return this.items[0];
+    for (let i = 0; i < this.items.length; i++) {
+      sumItems += this.items[i]
+    }
+    return sumItems / this.items.length
+  }
 }
 
 module.exports = SortedList;
